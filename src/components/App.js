a857de52beaf0b537e77c4297684166bb4e47321
Julie <djolie.hart@gmail.com>
2022-11-27 10:40:24 +0100

    Réorganisation des fichiers

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import Test from './Test.js';
+
+function App() {
+  return (<Test />);
+}
+
+export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,7 @@
+import '../styles/Test.css';
+
+function Test() {
+    return (<h1 className="title">Bonjour</h1>)
+}
+
+export default Test
